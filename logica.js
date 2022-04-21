bbde4e8a8b689105348e9e2d8d4e82ec006671b1
Breendairuela <103433282+Breendairuela@users.noreply.github.com>
2022-04-21 12:26:00 -0300

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,9 @@
+let nombre = "bren"
+let edad = 15;
+console.log (edad)
+prompt ("hola")
+alert ("hola2")
+let mensaje =prompt ("escriba un mensaje")
+console.log (mensaje);
+nombre =20;
+console.log (nombre)
